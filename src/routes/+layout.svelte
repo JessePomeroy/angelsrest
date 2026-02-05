8a2233f0787029cc619b2a7fc4deb673f8e7b0af
jessepomeroy <thinkingofview@gmail.com>
2026-02-05 00:24:10 -0500

    added a gif header on mobile devices

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,7 @@
 <script lang="ts">
   import type { Snippet } from "svelte";
+  import { page } from "$app/stores";
+  import headerGif from "$lib/assets/ponyolovesham.gif";
   import Nav from "$lib/components/Nav.svelte";
   import BottomNav from "$lib/components/BottomNav.svelte";
   import Footer from "$lib/components/Footer.svelte";
@@ -10,6 +12,13 @@
 
 <div class="flex flex-col min-h-screen">
   <Nav />
+  {#if $page.url.pathname !== "/"}
+    <div class="md:hidden">
+      <img src={headerGif} alt="" class="w-full" />
+    </div>
+    <div class="h-8 md:hidden"></div>
+  {/if}
+
   <main class="flex-1 max-w-[1400px] mx-auto w-full p-8 pb-24 md:pb-8">
     {@render children()}
   </main>
