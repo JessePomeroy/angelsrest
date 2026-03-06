d1eaea7270f9c35952e745716ff2a68cbe4d6d7e
jessepomeroy <thinkingofview@gmail.com>
2026-03-06 18:26:06 -0500

    Fix syntax error in period filter

--- src/routes/admin/orders/+page.svelte
+++ src/routes/admin/orders/+page.svelte
@@ -54,7 +54,8 @@
 					return false;
 				}
 			}
-		 >= range.end))	// Year filter
+
+			// Year filter
 			if (yearFilter !== 'all' && orderDate.getFullYear() !== parseInt(yearFilter)) {
 				return false;
 			}
