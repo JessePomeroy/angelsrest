87304fba1d9c52327fdf87f591c5914de92d3fef
jessepomeroy <thinkingofview@gmail.com>
2026-07-17 20:51:32 -0400

    Wire Blog supporting editor in Angels Rest

---
+++ src/routes/admin/editor/blog/categories/[documentId]/+page.svelte
@@ -0,0 +1,7 @@
+<script lang="ts">
+import { BlogSupportingPage } from "@jessepomeroy/admin";
+
+let { data } = $props();
+</script>
+
+<BlogSupportingPage documentId={data.params.documentId} kind="category" />
